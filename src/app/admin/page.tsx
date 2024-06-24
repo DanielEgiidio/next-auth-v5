c378e911228ad3c0e680223703e3198f9ea9e329
DanielEgiidio <danielegidiot@gmail.com>
2024-06-24 16:00:31 -0300

    feat: Admin role

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,34 @@
+import { auth } from "@/auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
-export default function Page() {
-  // TODO: Redirect non-admin users
+export default async function Page() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/admin");
+  }
+
+  if (user.role !== "admin") {
+    return (
+      <main className="mx-auto my-10">
+        <p className="text-center">
+          Você não está autorizado a ver esta página por que você não é um
+          Admnistrador
+        </p>
+      </main>
+    );
+  }
 
   return (
     <main className="mx-auto my-10 space-y-3">
       <h1 className="text-center text-xl font-bold">Admin Page</h1>
-      <p className="text-center">Welcome, admin!</p>
+      <p className="text-center">Bem vindo, administrador!</p>
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,33 @@
+import { auth, signIn } from "@/auth";
 import Link from "next/link";
+import UserButton from "./UserButton";
+import { Button } from "./ui/button";
 
-export default function NavBar() {
-  // TODO: Show the currently logged-in user
+export default async function NavBar() {
+  const session = await auth();
+  const user = session?.user;
 
   return (
     <header className="sticky top-0 bg-background px-3 shadow-sm">
       <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
         <Link href="/" className="font-bold">
-          Next-Auth v5 Tutorial
+          Next-Auth v5 
         </Link>
+        {user ? <UserButton user={user} /> : <SignInButton />}
       </nav>
     </header>
+  );
+}
+
+function SignInButton() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn();
+      }}
+    >
+      <Button type="submit">Sign in</Button>
+    </form>
   );
 }
